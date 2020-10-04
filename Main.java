4fb9c058dcfabb1df2e1a9676789b98785972283
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:55:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that will correctly name the animal based on the choices
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+  	Scanner input = new Scanner(System.in);
+		//asks the user if the animal has feathers
+		System.out.println("Does the animal have feathers?");
+		//gets the input from user
+		String animalFeather = input.nextLine();
+		//if the animal has feathers
+		if (animalFeather.equals("yes")){
+			//prompts user for wether it swims or not
+			System.out.println("Does the animal swim?");
+			String animalSwim = input.nextLine();
+			if (animalSwim.equals("yes")){
+				//if it swims its a duck
+				System.out.println("This is a duck");
+			}else {
+				//if not a hen
+				System.out.println("This is a hen");
+			}	
+		}else {//if the animal dont have feathers
+		//does it have legs
+			System.out.println("Does the animal have legs?");
+			String animalLegs = input.nextLine();
+			if (animalLegs.equals("yes")){
+				System.out.println("This is a lizard");
+			}else {
+				System.out.println("This is a snake");
+			}
+		}
   }
 }
